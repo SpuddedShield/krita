d83b583cf9efd4487c78cb898202006e95020055
Spudded shield <lucas.chiluc.v@outlook.com>
2026-05-11 15:49:39 -0400

    Delete resources directory

--- resources/types/mod.ts
+++
@@ -1,4 +0,0 @@
-export type * as PortsSchema from "./ports.schema.d.ts";
-export type * as CategoriesSchema from "./categories.schema.d.ts";
-export type * as PorcelainSchema from "./ports.porcelain.schema.d.ts";
-export type * as UserstylesSchema from "@catppuccin/userstyles/scripts/types/userstyles.d.ts";
